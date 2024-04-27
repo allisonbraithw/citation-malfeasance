16ab9eaf28f6c16c9de42796bfe67a8352e194d7
Allison Braithwaite <alliroseb@gmail.com>
2024-04-27 16:08:45 -0700

    work

--- app/main.py
+++ app/main.py
@@ -4,15 +4,21 @@ from fastapi import FastAPI
 
 # load dotenv
 from dotenv import load_dotenv
+from claim_extractor import ClaimExtractor
 
 load_dotenv()
 
 app = FastAPI()
 
 
-@app.post("/process/{item_id}")
-def process_paper():
+@app.post("/process/{file_name}")
+def process_paper(file_name: str):
     # load the paper
+    file_path = f"./data/{file_name}"
+    
+    claims = ClaimExtractor().extract_claims(file_path)
+    
+    print(claims)
     
     # open ai call to break down claims & citations
     return {"Hello": "World"}
