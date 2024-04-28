5ea62ad2ed088c8724b02c03f4b9358ec64ed6df
Allison Braithwaite <alliroseb@gmail.com>
2024-04-27 17:30:13 -0700

    specify json

--- app/claim_extractor.py
+++ app/claim_extractor.py
@@ -9,23 +9,28 @@ import os
 import hashlib
 import google.generativeai as genai
 from dotenv import load_dotenv
+import ast
+from pypdf import PdfReader
 
+from models import ClaimWithReferences
 
 load_dotenv()
 
 # genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-BASE_PROMPT = '''
-Identify the claims made in this paper and return a list of tuples, List[Tuple[claim , List<reference>]] . 
+BASE_PROMPT = f'''
+Identify the cited claims made in this paper and return a list of the claims, and the references provided to support them.
 
 For example, 
   [
     ("In order to leverage pre-trained unimodal models for VLP, it is key to facilitate cross-modal alignment. However, since
     LLMs have not seen images during their unimodal pretraining, freezing them makes vision-language alignment
     in particular challenging. In this regard, existing methods(<ref1>, <ref2> ) resort to an image-to-text generation loss,
-    which we show is insufficient to bridge the modality gap" , [Tsimpoukelli et al., 2021, Alayrac et al., 2022]]
+    which we show is insufficient to bridge the modality gap" , [Tsimpoukelli et al., 2021, Alayrac et al., 2022])
+  ]
+  
+Please only return a list of this json format {ClaimWithReferences(claim="example claim", references=["ref1", "ref2"]).model_dump_json()}
 '''
-
 
 # Python class for the claim extractor
 class ClaimExtractor:
@@ -34,24 +39,15 @@ class ClaimExtractor:
     self.model = genai.GenerativeModel(model_name=model_name)
     self.uploaded_files = []
 
-  def upload_if_needed(self, pathname: str) -> list[str]:
-    path = Path(pathname)
-    hash_id = hashlib.sha256(path.read_bytes()).hexdigest()
-    try:
-      existing_file = genai.get_file(name=hash_id)
-      return [existing_file.uri]
-    except:
-      pass
-    self.uploaded_files.append(genai.upload_file(path=path, display_name=hash_id))
-    return [self.uploaded_files[-1].uri]
-
   def extract_pdf_pages(self, pathname: str) -> list[str]:
     parts = [f"--- START OF PDF ${pathname} ---"]
-    # Add logic to read the PDF and return a list of pages here.
-    pages = []
-    for index, page in enumerate(pages):
+    pdf_reader = PdfReader(pathname)
+
+
+    for index, page in enumerate(pdf_reader.pages):
       parts.append(f"--- PAGE {index} ---")
-      parts.append(page)
+      parts.append(page.extract_text())
+    print(parts)
     return parts
 
   def extract_claims(self, pdf_path: str) -> str:
@@ -60,13 +56,20 @@ class ClaimExtractor:
         "role": "user",
         "parts": self.extract_pdf_pages(pdf_path)
       },
-      # {
-      #   "role": "user",
-      #   "parts": [BASE_PROMPT]
-      # },
+      {
+        "role": "user",
+        "parts": [BASE_PROMPT]
+      },
     ])
     convo.send_message(BASE_PROMPT)
-    print(convo.last)
+    print(convo.last.text)
+    # response = sample_response
+    # print(response)
+    # parse sample response into a list of ClaimWithReferences:
+    # claims = []
+    # for claim, references in ast.literal_eval(response):
+    #   claims.append(ClaimWithReferences(claim=claim, references=references))
+    # print(claims)
     return 'hi'
     # return convo.last.text
   
--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 # pydantic models for relevant objects
+from pydantic import BaseModel
+from typing import List, Tuple
 
 # model for Claim with references: List[Tuple[claim , List<reference>]]
+class ClaimWithReferences(BaseModel):
+    claim: str
+    references: List[str]
